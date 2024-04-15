69c2e0792b06d03954ee485e0ce2be63588c10b5
kanatsumi2003 <naduccva@gmail.com>
2024-04-15 17:21:40 +0700

    code moi

--- src/Application/Persistences/IRepositories/IUserRepository.ts
+++ src/Application/Persistences/IRepositories/IUserRepository.ts
@@ -1,7 +1,9 @@
+import { User } from "../../../Domain/Entities/UserEntites";
 import IBaseRepository from "./IBaseRepository";
 
 interface IUserRepository extends IBaseRepository {
-
+    createUser(user: User): Promise<void>;
+    login()
 }
 
 export default IUserRepository;
